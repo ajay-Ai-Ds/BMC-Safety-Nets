d7061ed1bf60fb9c3a13734c1f91b0f685370aba
Ajay <ajay@example.com>
2026-06-30 19:06:05 +0530

    sitemap updated

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://bmcsafetynets.in/sitemap.xml',
+    sitemap: 'https://bmcsafetynets.com/sitemap.xml',
   };
 }
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://bmcsafetynets.in',
+      url: 'https://bmcsafetynets.com',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1.0,
